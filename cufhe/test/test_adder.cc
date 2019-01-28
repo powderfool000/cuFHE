231677e68cef7538f63de14801d782e84765c900
Dominic <dfusco51@gmail.com>
2019-01-28 16:38:49 -0500

    Shift and MUX test

--- cufhe/test/test_adder.cc
+++ cufhe/test/test_adder.cc
@@ -47,36 +47,34 @@ void Mux(int* out, int* inp1, int* inp2, int sel, int n){
 }
 
 
-void sixteenMux(int* out, int in[][10], int* sel, int size, int n){
+void sixteenMux(int* out, int in[][13], int* sel, int size, int n) {
   int out1[n/2][size];
   int out2[n/4][size];
   int out3[n/8][size];
 
-cout <<"sixteen Mux in 0-16 is: "<<endl;
-//  for(int i =0; i < 16; i ++){
-//  cout<< in[i][i] << endl;
-//  }
- cout<< *in[1] << endl;
-cout <<"Mux 1: "<<endl;
-for(int i=0; i< ((n/2)); i++){
-    Mux(out1[i], in[i], in[(n/2) + i], sel[0], size);
-    cout<< out1[i][i] <<endl;
-}
-  cout<<"done one"<<endl;
-for(int i=0; i< ((n/4)); i++){
-   Mux(out2[i], out1[i], out1[(n/4) + i], sel[1], size);
- }
-for(int i=0; i< ((n/8)); i++){
-  Mux(out3[i], out2[i], out2[(n/8) + i], sel[2], size);
-}
-for(int i=0; i< ((n/16)); i++){
-  Mux(out, out3[0], out3[(n/16  + i)], sel[3], size);
-}
+  cout <<"sixteen Mux in 0-16 is: "<<endl;
+
+  for(int i=0; i < ((n/2)); i++) {
+  	Mux(out1[i], in[i], in[(n/2) + i], sel[3], size);
+  }
+
+  for(int i=0; i < ((n/4)); i++) {
+  	Mux(out2[i], out1[i], out1[(n/4) + i], sel[2], size);
+  }
+
+  for(int i=0; i < ((n/8)); i++) {
+  	Mux(out3[i], out2[i], out2[(n/8) + i], sel[1], size);
+  }
+
+  for(int i=0; i < ((n/16)); i++) {
+  	Mux(out, out3[0], out3[(n/16 + i)], sel[0], size);
+  }
 }
 
 
+//updated shift 
 void Shift(int smallout[], int smallin[], int smallzero, int n, int nshift) {
-	int temp = n - nshift;
+	int temp = (n - 1) - nshift;
 	for(int i = 0; i <= temp; i++) {
 		smallout[i] = smallin[nshift + i];
 	}
@@ -85,17 +83,47 @@ void Shift(int smallout[], int smallin[], int smallzero, int n, int nshift) {
 	}
 }
 
+void totalShift(int o[], int smallI[]){
+	int smallO[16][13];
+	int sel[4] = {0,1,0,0};
+
+	for(int i = 0; i <= 12; i++) {
+		if(i < 3){
+			Shift(smallO[i], smallI, 0, 13, i);
+		}
+		else {
+			Shift(smallO[i], smallI, 0 ,13, i);
+			smallO[i][2] = smallI[i-1];
+			smallO[i][1] = smallI[i-2];
+			smallO[i][0] = smallI[i-3];
+			smallO[i][0] = smallO[i][0] + smallO[i-1][0];
+		}
+	}
+
+	for (int i = 13; i < 16; i++){
+		for (int z = 0; z < 13; z++) {
+			smallO[i][z] = 0;
+		}
+	}
+
+	sixteenMux(o, smallO, sel, 13, 16);
+
+}
+
 
 
 int main() {
 	int inp[13] = {0,0,1,0,0,0,0,0,0,1,0,0,1};
 	int outp[13];
-	int num = 12;
-	int num_shift = 2;
-	int smallz = 0;
+	int out2p[13];
 
-	Shift(outp, inp, 0, num, num_shift);
+	Shift(out2p, inp, 0, 13, 2);
+	for(int i = 12; i >= 0; i--){
+		cout << out2p[i] << flush;
+	}
+	cout << endl;
 
+	totalShift(outp, inp);
 	for(int i = 12; i >= 0; i--){
 		cout << outp[i] << flush;
 	}
