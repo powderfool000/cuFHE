fd87b528f25fa728fe7daea6adc00953870d6b4b
ChobbyRace <RobC697@gmail.com>
2019-01-30 14:43:53 -0500

    Normalize and Part 3

--- cufhe/test/test_adder.cc
+++ cufhe/test/test_adder.cc
@@ -56,6 +56,24 @@ void Rca(int* z, int* co, int* a, int* b, int n) {
   }
 }
 
+ void RCS(int* z, int* co, int* a, int* b, int n){
+    int tempb[n];
+    int finalb [n];
+    int one[n];
+    //invert
+  for(int i =0; i<n; i++){
+    tempb[i] = b[i] ^ 1;
+  }
+
+  one[0] = 1;
+
+  for(int i =1; i<n; i++){
+    one[i] = 0;
+  }
+  Rca(finalb,co, one, tempb ,n);
+  Rca(z,co,a,finalb,n);
+}
+
 void Mux(int* out, int* inp1, int* inp2, int sel, int n){
   int p0[n];
   int p1[n];
@@ -72,38 +90,14 @@ void Mux(int* out, int* inp1, int* inp2, int sel, int n){
   }
 }
 
- void RCS(int* z, int* co, int* a, int* b, int n){
-    int tempb[n];
-  for(int i =0; i<n; i++){
-    tempb[i] = b[i] ^ 1;
-  }
-  int one[n];
-  one[0] = 1;
-  for(int i =1; i<n; i++){
-    one[i] = 0;
-
-  }
-  int finalb [n];
-  Rca(finalb,co, one, tempb ,n);
-  Rca(z,co,a,finalb,n);
-}
-
 void sixteenMux(int* out, int in[][10], int* sel, int size, int n){
   int out1[n/2][size];
   int out2[n/4][size];
   int out3[n/8][size];
-
-cout <<"sixteen Mux in 0-16 is: "<<endl;
-//  for(int i =0; i < 16; i ++){
-//  cout<< in[i][i] << endl;
-//  }
- cout<< *in[1] << endl;
-cout <<"Mux 1: "<<endl;
 for(int i=0; i< ((n/2)); i++){
     Mux(out1[i], in[i], in[(n/2) + i], sel[0], size);
     cout<< out1[i][i] <<endl;
 }
-  cout<<"done one"<<endl;
 for(int i=0; i< ((n/4)); i++){
    Mux(out2[i], out1[i], out1[(n/4) + i], sel[1], size);
  }
@@ -115,7 +109,23 @@ for(int i=0; i< ((n/16)); i++){
 }
 }
 
+void roundNormalize(int* finalSum, int* tempexpoCo, int* mantisaCosum, int* co, int* mantisaSum, int* expoOne, int* tempexpo, int* smallZero){
+  Shift(mantisaCosum, mantisaSum, smallZero, 10, 1, st);   // if carry out, shift mantisa right by 1    ***note, may be interpretting "product" wrong in the algorithm description***
+  Rca(tempexpoCo, co, tempexpo, expoOne, 5);  // expoOne is just a 5 bit number 00001 to add to tempexpo
+  Mux(tempexpo, tempexpoCo, tempexpo, co, 5); //chose to use the added exponent or not
+  Mux(mantisaSum, mantisaCosum, mantisaSum, co, 13); //chosing the correct mantisa
+  for(int i=0; i<19; i++){
+    if(i<13){
+      finalSum[i] = mantisaSum[i];
+    }
+    else{
+      finalSum[i] = tempexpo[i+13];
+    }
+  }
+}
+
 void floatAdder(int* out, int* in1, int* in2, int* test){
+    //part 1
     int in1exp[5];
     int in2exp[5];
     int in1mantisaR[13];
@@ -130,14 +140,30 @@ void floatAdder(int* out, int* in1, int* in2, int* test){
     int bigInman[13];
     int exposum[5];
 
+    //part2 
+    ///
+    //part 3
+    int smallZero = 0;
+    int mantisaSum[13]; //sum of the two mantisas
+    int* manCo; //carryout
+
+
+
   for(int i = 0; i < 5; i++){
     in1exp[i] = in1[10+i];
     in2exp[i] = in2[10+i];
   }
-  for(int i = 0; i< 10; i++){             
-    in1mantisaR[i+3] = in1[i];  //leae last 3 bits for the round bits
-    in2mantisaR[i+3] = in2[i];
+  for(int i = 0; i< 13; i++){  
+    if(i<3){
+      in1mantisaR[i] = 0;  //leae last 3 bits for the round bits
+      in2mantisaR[i] = 0;
+    }  
+    else{         
+      in1mantisaR[i] = in1[i];  //leae last 3 bits for the round bits
+      in2mantisaR[i] = in2[i];
+    }
   }
+
   cout<< "exposum is:" << endl;
   RCS(exposum,co, in1exp, in2exp, 5);
   for(int i=0;i<5;i++){
@@ -153,23 +179,28 @@ void floatAdder(int* out, int* in1, int* in2, int* test){
   Mux(bigIn, in1, in2, negcheck, 16);            // which input is bigger
   Mux(smallInman, in2mantisaR, in1mantisaR, negcheck, 13); //chose the smaller mantisa
   Mux(bigInman, in1mantisaR, in2mantisaR,  negcheck, 13);    //chose the larger mantisa
+  //part 2
+  //////////////////
+  // part 3 
+  Rca(mantisaSum, manCo, smallInman, bigInman, 13);
 
-  //testing line
+  roundNormalize(finalSum, tempexpoCo, mantisaCosum, co, mantisaSum, expoOne, tempexpo, smallZero)
+    //testing line
   for(int i =0; i< 13; i++){
-     test[i] = bigInman[i];
+     test[i] = mantisaSum[i];
   }
- 
 }
 int main() {
  int inp1[16] = {0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0};
  int inp2[16] = {1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0};
  int out[16];
  int test[13];
- // cout<< "Testing Part 1" << endl;
- // floatAdder(out, inp1, inp2, test);
- // cout<<"big mantisa is:" <<endl;
- //  for(int i = 0; i < 13; i ++){
- //   cout << test[i] << flush;
- // }
+ cout<< "Testing Part 1" << endl;
+ floatAdder(out, inp1, inp2, test);
+ cout<<"mantis sum is:" <<endl;
+  for(int i = 0; i < 13; i ++){
+   cout << test[i] << flush;
+ }
+ cout<<""<<endl;
  return 0;
 }
