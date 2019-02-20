2a7ed28e29e6cc3930a0e6adb35bee5b5167f381
ChobbyRace <RobC697@gmail.com>
2019-02-20 14:02:20 -0500

    15 CSA test

--- cufhe/test/test_adder_gpu.cu
+++ cufhe/test/test_adder_gpu.cu
@@ -55,7 +55,7 @@ int8_t dump_ptxt(Ptxt* p, uint8_t n) {
 
 int main() {
   uint8_t N = 4;
-  uint8_t tests = 5;
+  uint8_t tests = 15;
   uint8_t ns = N;
 
   SetSeed();  // set random seed
@@ -95,8 +95,8 @@ int main() {
   cout<< "------ ALU Test ------" <<endl;
 
   init_ptxt(pts, 1, 1);
-  init_ptxt(pta, 8, N);
-  init_ptxt(ptb, 2, N);
+  init_ptxt(pta, 1, N);
+  init_ptxt(ptb, 1, N);
 
   cout<<"A: "<<int(dump_ptxt(pta, N))<<endl;
   cout<<"B: "<<int(dump_ptxt(ptb, N))<<endl;
